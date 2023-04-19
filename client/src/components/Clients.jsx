f3b7415f265fa6ca838beed04bb4fe007d7931f1
Gerald Waga <106008160+geraldWaga92@users.noreply.github.com>
2023-04-19 16:34:10 -0700

    update Clients component with fetching data into frontend

--- client/src/components/Clients.jsx
+++ client/src/components/Clients.jsx
@@ -1,29 +1,39 @@
-import { gql, useQuery } from "@apollo/client"
+import { gql, useQuery } from "@apollo/client";
+import ClientRow from "./ClientRow.jsx";
 
 const GET_CLIENTS = gql`
-    query getClients {
-        clients {
-            id
-            name
-            email
-            phone
-        }
+  query getClients {
+    clients {
+      id
+      name
+      email
+      phone
     }
-`
+  }
+`;
 
 export default function Clients() {
+  const { loading, error, data } = useQuery(GET_CLIENTS);
 
-    const { loading, error, data } = useQuery(GET_CLIENTS)
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Something went wrong!</p>;
 
-    if(loading) return <p>Loading...</p>
-    if (error) return <p>Something went wrong!</p>
-    
-    
-  return (
-    <>
-        { !loading && !error (
-            <h1>Cients</h1>
-        ) }
-    </>
-  )
+  return <>{!loading && !error && (
+    <table className="table table-hover mt-3">
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Phone</th>
+                <th></th>
+            </tr>
+        </thead>
+
+        <tbody>
+            {data.clients.map(client => (
+                <ClientRow key={client.id} client={client} />
+            ))}
+        </tbody>
+    </table>
+  )}</>;
 }
--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 // eslint-disable-next-line no-unused-vars
 const colors = require('colors');
+const cors = require('cors')
 require('dotenv').config()
 const { graphqlHTTP } = require('express-graphql')
 const schema = require('./schema/schema')
@@ -8,6 +9,8 @@ const connectDB = require('./config/db')
 
 const PORT = process.env.PORT || 4000
 const app = express()
+
+app.use(cors())
 
 // connect to database
 connectDB();
