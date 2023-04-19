8cdff08b9f888ec661355953fa5922675640fb78
Gerald Waga <106008160+geraldWaga92@users.noreply.github.com>
2023-04-19 15:59:13 -0700

    create header

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,16 @@
+import Logo from "./assets/logo.png"
+
+export default function Header() {
+  return (
+    <nav className="navbar bg-light mb-4 p-0">
+      <div className="container">
+        <a className="navbar-brand" href="/">
+          <div className="d-flex">
+            <img src={Logo} alt="Logo" className="mr-2" />
+            <div>Project Management App</div>
+          </div>
+        </a>
+      </div> 
+    </nav>
+  );
+}
